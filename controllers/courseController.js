66f84e8c423269005fdb2b94d001ea470ebb9aa6
Kris <9ut4@fresnouorg>
2024-02-09 09:28:05 -0800

    initial commit

---
+++ controllers/courseController.js
@@ -0,0 +1,41 @@
+const {Course} = require('../models')
+const departments = ['Math', 'English', 'Music', 'Art','PE', 'World Languages', 'Social Studies', 'Science'].sort();
+
+//view all
+module.exports.viewAll = async function(req,res){
+    const courses = await Course.findAll();
+    res.render('course/view_all', {courses});
+}
+
+//profile
+module.exports.viewProfile= async function(req,res){
+    const course = await Course.findByPk(req.params.id);
+    res.render('course/profile', {course})
+}
+
+//render add form
+
+//add
+
+//render edit form
+module.exports.renderEditForm = async function(req,res){
+    const course = await Course.findByPk(req.params.id);
+    res.render('course/edit', {course, departments});
+}
+
+//update
+module.exports.updateCourse = async function(req,res){
+    const course = await Course.update({
+        name: req.body.name,
+        department: req.body.department,
+        instructor_name: req.body.instructor_name,
+        description: req.body.description
+    }, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect(`/course/profile/${req.params.id}`);
+}
+
+//delete
